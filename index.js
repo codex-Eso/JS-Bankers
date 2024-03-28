fdfd285b5a2322de836c04580feaeb10811be163
codex-Eso <161030447+codex-Eso@users.noreply.github.com>
2024-03-28 21:30:42 +0800

    UPDATE V2

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
-/*Emotes: 💰 💲 ✅ 💸 🤑 📊 🏃 💰 🫴*/
+/*Emotes: 💰 💲 ✅ 💸 🤑 📊 🏃 💰 🫴 🫰*/
 /* Things for Updates:
 New (v2):
-- Adding tutorial
-- Adding market
-- Adding new items (including op items)
-- Adding cards
-- Help text (styling)
-- Materials to build items (add craft button)
-- Materials found in find
-Rework (v1.5):
-✅ Help text (last)
-✅ Removal of earn button, only auto earn
-✅ Removal of lottery button
-✅ Removal of rob button
-✅ Auto earn per sec (decrease whenever you get a worker by -0.01 seconds)
-✅ Limit hired workers to 500
-✅ Start with 100 JS dollars
-✅ Removal of speedy gloves
-✅ Removal of codes
-✅ Removal of LunchBox and Speedy Glove
-✅ Rework purse money
-✅ Adding new button, FIND (needs 10 worker to find op item)
-✅ Reworked invest so that it does not give total money to be negative
+✅ Adding tutorial (last)
+✅ Adding market (currently working)
+✅ Add inventory button
+✅ Adding new items (including op items)
+    ✅ Robot 
+    ✅ LunchBox (Torn recipe part a📜, torn recipe part b📜, sandwich🥪)
+    ✅ Trendy Watch (Clock piece, trendy market)
+    ✅ Rich Hat (hat👒, golden coin🪙, scarf🧣)
+✅ Help text (styling) (last) (mention how to craft in help)
+✅ Materials to build items (add craft button)
+✅ Materials found in find
+✅ Adding cards
+    ✅ Number 1 card = ♦ * 3 (Decrease the timer for auto from 3 to 0 - 1000)
+    ✅ Money-Money card = ♥ * 3
+    ✅ Trendy card = ♠ * 3
+    ✅ Free labour card = ♣ * 3
+Rework:
+✅ Remove congrats message for every 100 JS dollars earned
+✅ Improve speed of game
+    - Earn + 5 every 3s = Decrease limit of workers from 500 to 100
+- Rework main goal : Achievements like Game Beta (21)
+    ✅ The Unlucky (3 in a row)
+    ✅ The Lucky (Get op item)
+    ✅ The Seller (Sell one item)
+    ✅ The Buyer (Buy one item)
+    ✅ The Rich one who always buys stuff 🙄 (buy 5 items)
+    ✅ The Reseller? (sell 5 items)
+    ✅ The Robber (Rob the market)
+    ✅ The CraftsMan (craft 1 item)
+    ✅ The Superior Crafter (craft 4 items)
+    ✅ Tutorial (Pass the tutorial)
+    ✅ Up To Trend (Get a trendy watch)
+    ✅ Maxed Trend (Sell your trendy watch)
+    ✅ The Collector (get 5 items found in auto)
+    ✅ The Card Collector (get 9 cards)
+    ✅ You had a four leaf clover, did you? (get the golden coin)
+    ✅ SPECIAL DEAL! (get the mechanical part)
+    ✅ Risk Taker (invest >= 10000 JS)
+    ✅ There Is An Achievement For This (Use help, then type achievements)
+    ✅ Enough Is Enough (Get the max no. of workers)
+    ✅ The Social (click the discord button)
+    ✅ The TryHard (Get all achievements)
+    ✅ Curiousity (find the special button)
+    ✅ 1 in 10 (get 100k JS dollars)
+    ✅ Millionaire (get 1m JS dollars)
+✅ Increase luck in getting items from auto
+✅ Decrease luck in getting materials from find
+✅ Change requirement for find from 10 to 20 workers
 Bug fixes:
-✅ Fixed investment and hire input bug
-✅ Fixed item for congrats
+✅ Bug in find where it gives you negative money
+✅ Bug fixed in help button (wrong text)
 */
