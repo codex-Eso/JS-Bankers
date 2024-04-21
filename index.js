ebccc954603f591d328f987f1dcddbb5b8915f57
codex-Eso <161030447+codex-Eso@users.noreply.github.com>
2024-04-21 21:22:20 +0800

    Update 2.7

--- index.js
+++ index.js
@@ -1,55 +1,14 @@
-/*Emotes: 💰 💲 ✅ 💸 🤑 📊 🏃 💰 🫴 🫰*/
-/* Things for Updates:
-New (v2):
-✅ Adding tutorial (last)
-✅ Adding market (currently working)
-✅ Add inventory button
-✅ Adding new items (including op items)
-    ✅ Robot 
-    ✅ LunchBox (Torn recipe part a📜, torn recipe part b📜, sandwich🥪)
-    ✅ Trendy Watch (Clock piece, trendy market)
-    ✅ Rich Hat (hat👒, golden coin🪙, scarf🧣)
-✅ Help text (styling) (last) (mention how to craft in help)
-✅ Materials to build items (add craft button)
-✅ Materials found in find
-✅ Adding cards
-    ✅ Number 1 card = ♦ * 3 (Decrease the timer for auto from 3 to 0 - 1000)
-    ✅ Money-Money card = ♥ * 3
-    ✅ Trendy card = ♠ * 3
-    ✅ Free labour card = ♣ * 3
+/*Fixing :
+✅Discord Bug (fixed)
+✅Craft message bug (fixed)
+✅Lucky achievement bug (fixed)
+✅Fix inventory bug (removal of items in inv)
 Rework:
-✅ Remove congrats message for every 100 JS dollars earned
-✅ Improve speed of game
-    - Earn + 5 every 3s = Decrease limit of workers from 500 to 100
-- Rework main goal : Achievements like Game Beta (21)
-    1. The Unlucky (3 in a row)
-    2. The Lucky (Get op item)
-    3. The Seller (Sell one item)
-    4. The Buyer (Buy one item)
-    5. The Rich one who always buys stuff 🙄 (buy 5 items)
-    6. The Reseller? (sell 5 items)
-    7. The Robber (Rob the market)
-    8. The CraftsMan (craft 1 item)
-    9. The Superior Crafter (craft 4 items)
-    10. Tutorial (Pass the tutorial)
-    11. Up To Trend (Get a trendy watch)
-    12. Maxed Trend (Sell your trendy watch)
-    13. The Collector (get 5 items found in auto)
-    14. The Card Collector (get 9 cards)
-    15. You had a four leaf clover, did you? (get the golden coin)
-    16. SPECIAL DEAL! (get the mechanical part)
-    17. Risk Taker (invest >= 10000 JS)
-    18. There Is An Achievement For This (Use help, then type achievements)
-    19. Enough Is Enough (Get the max no. of workers)
-    20. The Social (click the discord button)
-    21. The TryHard (Get all achievements)
-    22. Curiousity (find the special button)
-    23. 1 in 10 (get 100k JS dollars)
-    24. Millionaire (get 1m JS dollars)
-✅ Increase luck in getting items from auto
-✅ Decrease luck in getting materials from find
-✅ Change requirement for find from 10 to 20 workers
-Bug fixes:
-✅ Bug in find where it gives you negative money
-✅ Bug fixed in help button (wrong text)
-*/
+✅Fixes in the texts for tutorial
+✅Requires 1000 JS dollars to use find
+✅Balance Changes in some item prices
+✅Rework achievement text
+New:
+✅Market allows you to sell your extra items in your inventory! (each for 2500)
+✅Added 1 new achievement
+-Achievement bug (fixing when adding logging in)*/
